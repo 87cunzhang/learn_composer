2c711f2c5f91fea573ae2337e4149fede456a428
87cunzhang <2423561050@qq.com>
2021-01-08 11:34:45 +0800

    .

---
+++ Tools/Baidu/Maps.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by 村长
+ * Date: 2021-01-08
+ * Time: 11:33
+ */
+
+namespace Tools\Baidu;
+
+
+class Maps
+{
+    public function go()
+    {
+        return 'go to beijing';
+    }
+}
